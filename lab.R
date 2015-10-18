3023357504d381337a30eac6d538cee4b42f21f0
daniahtahir <daniah.tahir@math.uu.se>
2015-10-18 23:39:39 +0200

    integrate

--- lab.R
+++ lab.R
@@ -1,11 +1,18 @@
-y <- function(x) {(x-3)^2+2*(x-3)^2+3*(x-15)^2+sin(100*x)}
+#OPTIMISE
 
-optimise(y, c(0,100))
-optimise(y, c(0,15))
-optimise(y, c(9,12))
-optimise(y, c(10,11))
+y1 <- function(x) {(x-3)^2+2*(x-3)^2+3*(x-15)^2+sin(100*x)}
 
-plot(y,0,30)
+optimise(y1, c(0,100))
+optimise(y1, c(0,15))
+optimise(y1, c(9,12))
+optimise(y1, c(10,11))
+plot(y1,0,30)
 
 # we do not get the same optimum each time
 # we get local optimums (not global optimum)
+
+#INTEGRATE
+
+y2 <- function(x) {x*sin(x)}
+integrate(y2, lower = -7e5, upper = 7e5, subdivisions=1e7)
+system.time(integrate(y2, lower = -7e5, upper = 7e5, subdivisions=1e7))
