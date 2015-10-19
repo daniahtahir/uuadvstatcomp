5d1f7e43088b14c6f92c1098c40907c36e6a3977
daniahtahir <daniah.tahir@math.uu.se>
2015-10-19 04:31:36 +0200

    parallel

--- lab.R
+++ lab.R
@@ -1,18 +1,34 @@
 #OPTIMISE
 
-y1 <- function(x) {(x-3)^2+2*(x-3)^2+3*(x-15)^2+sin(100*x)}
+y <- function(x) {(x-3)^2+2*(x-3)^2+3*(x-15)^2+sin(100*x)}
 
-optimise(y1, c(0,100))
-optimise(y1, c(0,15))
-optimise(y1, c(9,12))
-optimise(y1, c(10,11))
-plot(y1,0,30)
+optimise(y, c(0,100))
+optimise(y, c(0,15))
+optimise(y, c(9,12))
+optimise(y, c(10,11))
+plot(y,0,30)
 
 # we do not get the same optimum each time
 # we get local optimums (not global optimum)
 
 #INTEGRATE
 
-y2 <- function(x) {x*sin(x)}
-integrate(y2, lower = -7e5, upper = 7e5, subdivisions=1e7)
-system.time(integrate(y2, lower = -7e5, upper = 7e5, subdivisions=1e7))
+function(x) {x*sin(x)}
+integrate(function(x) {x*sin(x)}, lower = -7e5, upper = 7e5, subdivisions=1e7)
+system.time(integrate(function(x) {x*sin(x)}, lower = -7e5, upper = 7e5, subdivisions=1e7))
+
+#PARALLEL
+
+#make subintervals of the given interval (-7e5,7e5)
+
+library(parallel)
+cl <- makePSOCKcluster(16)  #1.56
+system.time(parLapply(cl,(-7:7)*1e5,(function(x) { integrate(function (x) { x*sin(x) }, x, x+1e5, subdivisions = 1e7)})))
+stopCluster(cl)
+
+
+
+
+
+
+
